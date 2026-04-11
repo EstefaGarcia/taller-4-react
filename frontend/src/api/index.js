dcb1ef441d636762ac4af80755875a11417e70d4
yonier viveros <viverosyonier3@gmail.com>
2026-04-11 17:40:30 -0500

    fix: agregar /api al baseURL

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://gastos-app-3.onrender.com"
+  baseURL: "https://gastos-app-3.onrender.com/api" // 🔥 AQUÍ
 })
 
 API.interceptors.request.use((config) => {
