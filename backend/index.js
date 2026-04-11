1c1095d99595058359f55cf83439eb16ac9efac8
yonier viveros <viverosyonier3@gmail.com>
2026-04-11 16:56:28 -0500

    soy un pendejo

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,6 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 
-import mongoose from "mongoose"
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB conectado"))
-  .catch(err => console.log(err))
 
 
 import authRoutes from "./routes/authRoutes.js"
