641823c7b4bd08291388f7729f7553419d6ed273
yonier viveros <viverosyonier3@gmail.com>
2026-04-12 12:04:10 -0500

    cambios4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,46 +7,51 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: [  'pwa-192x192.png', 'favicon.svg', 'apple-touch-icon.png', 'mask-icon.svg',  'robots.txt'],
-      workbox:{
-        navigateFallback:"/index.html",
-        globPatterns: ["**/.{jsx,css,html,png,jpg,ico,txt}"]
 
+      includeAssets: [
+        'pwa-192x192.png',
+        'favicon.svg',
+        'apple-touch-icon.png',
+        'mask-icon.svg',
+        'robots.txt'
+      ],
+
+      workbox: {
+        navigateFallback: '/index.html',
+
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,jpg,svg,txt}'
+        ]
       },
+
       manifest: {
         name: 'Aplicacion de gastos diarios',
         short_name: 'Gastos',
-        description: 'Aplicación para registrar gastos gastos diarios',
-        start_url: "/",
-        display: "standalone",
+        description: 'Aplicación para registrar gastos diarios',
+        start_url: '/',
+        display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#000',
 
-        screenshots:[
+        screenshots: [
           {
-          src: "/image/logo.png",
-          sizes: "350x350",
-          type: "image/png",
-          form_factor: "wide"
+            src: '/image/logo.png',
+            sizes: '350x350',
+            type: 'image/png',
+            form_factor: 'wide'
           },
           {
-               src: "/image/logo.png",
-              sizes: "350x350",
-             type: "image/png",
-            form_factor: "narrow"
-        }
+            src: '/image/logo.png',
+            sizes: '350x350',
+            type: 'image/png',
+            form_factor: 'narrow'
+          }
         ],
-
 
         icons: [
           {
             src: 'pwa-192x192.png',
-            sizes: '950x950',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '950x950',
+            sizes: '192x192',
             type: 'image/png'
           }
         ]
